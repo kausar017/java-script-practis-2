aa4c96eb98da57651d986723a23cb01cc9000d82
kousra <mdkousarmia71@gmail.com>
2024-09-13 19:44:24 +0600

    java script peactis

--- array.js
+++ array.js
@@ -1,4 +1,15 @@
 const numbars = [34,55,85,95,45,68,52,88,55];
-// console.log(numbars.length);
-const x = numbars[2];
+console.log(numbars.length);
+ 
+console.log(numbars);
+numbars.push(100,200);
+numbars.pop()
+const x = numbars.pop();
+console.log(numbars);
 console.log(x);
+
+
+const fruits = ['amm', 'jam', 'katal', 'Bannna'];
+const flowers = ['golap','sapla', 'joba', 'bali', 'rojonigonda'];
+const add = fruits.concat(flowers);
+console.log(add);
